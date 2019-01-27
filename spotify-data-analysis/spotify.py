a897b0175943dff787f5d62c3f85b6566faecb2a
Jonny <spicerjonny@gmail.com>
2019-01-27 14:56:22 +0000

    read json and output as pandas dataframe

--- spotify-data-analysis/spotify.py
+++ spotify-data-analysis/spotify.py
@@ -1,7 +1,17 @@
 import json
+import pandas as pd
+import datetime
+import pandas_datareader.data as web
+# import matplotlib.pyplot as plt
+# from matplotlib import style
+from pandas.io.json import json_normalize
 from pprint import pprint
 
-with open('data.json', encoding="utf-8") as f:
-    data = json.load(f)
+# style.use('fivethirtyeight')
 
-pprint(data)
+with open('data.json') as f:
+    data = json.load(f)
+    json_normalize(data)
+    data = pd.DataFrame.from_dict(data['songs'], orient='columns')
+    data.reset_index(level=0, inplace=True)
+    print(data)
